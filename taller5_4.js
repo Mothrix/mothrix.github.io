5f7bf0bd314267037f28831081d61b0f99c9375b
Mothrix <cintiapergar@gmail.com>
2024-08-20 20:03:13 -0300

    Add files via upload

---
+++ taller5_4.js
@@ -0,0 +1,12 @@
+function saludar (){
+    alert
+("bienvenid@");
+}
+saludar();
+
+var resultado = window.confirm('Estas list@ para la prueba?');
+if (resultado === true) {
+    window.alert('S!! Exitos!');
+} else { 
+    window.alert('Uhhh! a Repasar!');
+}
